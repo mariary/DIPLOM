86eb7694dc54f9f8cadc391222026ec59b207b88
Maria <Mrybakonenko>
2022-01-31 17:28:10 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <h1>kekeek </h1>
+                <h1>kekeeksfsdf sf </h1>
             </div>
         );
     }
